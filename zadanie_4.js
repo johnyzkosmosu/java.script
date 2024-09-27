b4a57dc7eb1263e68b3d7331189e3320d35f36c7
johnyzkosmosu <144529615+johnyzkosmosu@users.noreply.github.com>
2024-09-27 14:02:00 +0200

    Create zadanie_4.js

---
+++ zadanie_4.js
@@ -0,0 +1,27 @@
+<html>
+ <head>
+ <title> Łukowski 3G</title>
+ <script language="JavaScript">
+ function WinOpen_z3() 
+ {
+window.open("zadanie_3_Łukowski.html","okienko_z3","toolbar=no,directories=no,menubar=no,height=380,width=160,top=200,left=200");
+ }
+function WinOpen_z4() 
+ {
+window.open("zadanie_4_Łukowski.html","okienko_z4","toolbar=no,directories=no,menubar=no,height=380,width=160,top=200,left=400");
+ }
+function okno_zamknij() 
+ {
+ window.close()
+ }
+ </script>
+ </head>
+<body>
+ <form>
+ Szkielet zaliczeniowy Java Script Jan Łukowski 3G <br>
+ <input type="button_z3" name="zadanie3" value="zadanie_3_Łukowski" onclick="WinOpen_z3(' ')">
+ <input type="button_z4" name="zadanie4" value="zadanie_4_Łukowski" onclick="WinOpen_z4(' ')">
+ <input type="button" value="zamknij okno" onclick="okno_zamknij()"/>
+ </form>
+</body>
+</html>
